0e98ef87d338f5916c333a285b0e3c418dd1ff5e
Evan DeLaney <evan@thinkfishhook.com>
2014-10-17 02:26:21 -0400

    Initial Commit

---
+++ Core Bluetooth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Core Bluetooth
+//
+//  Created by Evan DeLaney on 10/17/14.
+//  Copyright (c) 2014 Fish Hook LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Core Bluetooth/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Core Bluetooth
+//
+//  Created by Evan DeLaney on 10/17/14.
+//  Copyright (c) 2014 Fish Hook LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
